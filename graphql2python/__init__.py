9958fc399a7712b7ab5ca13b4aefdbda35e485e9
Denis A. Artyushin <artyushinden@gmail.com>
2022-11-28 12:58:41 +0300

    build: Add simple package

---
+++ graphql2python/__init__.py
@@ -0,0 +1,24 @@
+"""GRAPHQL2PYTHON
+
+Tools for GraphQL client in python:
+
+    - A simple GraphQL client;
+    - Auto generated pydantic classes from a GraphQL schema;
+    - Auto generated GraphQL queries;
+    - e.t.c
+
+The sub-packages:
+
+    - `graphql2python.client`:
+    - `graphql2python.model`:
+    - `graphql2python.query`:
+
+"""
+
+from .__version__ import __version__
+from .__info__ import (
+    __email__,
+    __author__,
+    __license__,
+    __maintainer__,
+)
