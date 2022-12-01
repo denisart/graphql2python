315a66524ac4d4ad0bd987f00f44fc8bcc1bb4ad
Denis A. Artyushin <artyushinden@gmail.com>
2022-12-01 18:03:28 +0300

    feature: Add validation of GraphQL2PythonModelConfig
    - validation schema
    - validation output python file

--- graphql2python/model/config.py
+++ graphql2python/model/config.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
+from pathlib import Path
 
 __all__ = [
     "GraphQL2PythonModelOptions",
@@ -22,8 +23,31 @@ class GraphQL2PythonModelOptions(BaseModel):
 class GraphQL2PythonModelConfig(BaseModel):
     """GraphQL2Python config for pydantic data-model generation."""
 
-    schema: str = Field(description="A path to the target GraphQL schema file.")
-    output: str = Field(description="A path to an output python file.")
+    graphql_schema: Path = Field(description="A path to the target GraphQL schema file.", alias="schema")
+    output: Path = Field(description="A path to an output python file.")
     options: GraphQL2PythonModelOptions = Field(
         description="Data-model render options.", default=GraphQL2PythonModelOptions()
     )
+
+    @validator("graphql_schema")
+    def validation_graphql_schema_file(cls, v: Path):
+        if not v.exists():
+            raise ValueError(f"The file {v} is not exist.")
+
+        if not v.is_file():
+            raise ValueError(f"The input schema is not file.")
+
+        if v.suffix != ".graphql":
+            raise ValueError(f"The input file must have the suffix is .graphql")
+
+        return v
+
+    @validator("output")
+    def validation_output_py_file(cls, v: Path):
+        if v.exists():
+            raise ValueError(f"The file {v} is exist.")
+
+        if v.suffix != ".py":
+            raise ValueError(f"The output file must have the suffix is .py")
+
+        return v
