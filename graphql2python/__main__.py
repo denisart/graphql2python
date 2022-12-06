58c3cb702a3ca8448bc11939ea383076302c35f9
Denis A. Artyushin <artyushinden@gmail.com>
2022-12-06 12:44:24 +0300

    feature: Code style fix

--- graphql2python/__main__.py
+++ graphql2python/__main__.py
@@ -1,31 +1,11 @@
 import os
 from pathlib import Path
-from typing import Optional
 
 import click
 import yaml
 
 from graphql2python.model.config import GraphQL2PythonModelConfig
 from graphql2python.model.generate import Generator
-
-
-class YamlLoadError(Exception):
-    """An exception for load error of yaml file."""
-
-
-def load_yaml(filename: str, encoding: Optional[str] = None):
-    """Load some yaml file."""
-
-    try:
-        with open(filename, 'r', encoding=encoding) as fh:
-            return yaml.safe_load(fh)
-
-    except (OSError, yaml.YAMLError, UnicodeDecodeError) as error:
-        if encoding is None:
-            # try with default encoding UTF-8
-            return load_yaml(filename, encoding='utf-8')
-
-        raise YamlLoadError(f'Load Yaml failed: {error}') from error
 
 
 @click.group()
@@ -45,8 +25,8 @@ def generate(config: str):
     cwd_path = Path(os.getcwd())
     config_path = (cwd_path / Path(config)).resolve()
 
-    with config_path.open("r", encoding="utf-8") as fh:
-        yaml_config = yaml.safe_load(fh)
+    with config_path.open("r", encoding="utf-8") as config_file:
+        yaml_config = yaml.safe_load(config_file)
 
     if ("schema" not in yaml_config) or ("output" not in yaml_config):
         raise ValueError("Missing config fields: schema or output.")
