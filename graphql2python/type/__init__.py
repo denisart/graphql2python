0afb50d5c688ae23b4de7a0244985885a0d076f3
Denis Artyushin <artyushinden@gmail.com>
2023-03-23 14:31:42 +0300

    feature: Fixed imports

--- graphql2python/type/__init__.py
+++ graphql2python/type/__init__.py
@@ -1,13 +1,25 @@
 from .definitions import (
-    GraphQL2PythonEnumType,
-    GraphQL2PythonEnumValue,
+    GraphQL2PythonFieldSequenceItemType,
     GraphQL2PythonScalarType,
-    GraphQL2PythonUnionType
+    GraphQL2PythonField,
+    GraphQL2PythonObject,
+    GraphQL2PythonInterfaceType,
+    GraphQL2PythonObjectType,
+    GraphQL2PythonEnumValue,
+    GraphQL2PythonEnumType,
+    GraphQL2PythonUnionType,
 )
+from .schema import GraphQL2PythonSchema
 
 __all__ = [
+    "GraphQL2PythonFieldSequenceItemType",
     "GraphQL2PythonScalarType",
+    "GraphQL2PythonField",
+    "GraphQL2PythonObject",
+    "GraphQL2PythonInterfaceType",
+    "GraphQL2PythonObjectType",
     "GraphQL2PythonEnumValue",
     "GraphQL2PythonEnumType",
     "GraphQL2PythonUnionType",
+    "GraphQL2PythonSchema",
 ]
