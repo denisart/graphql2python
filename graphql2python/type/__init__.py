3d25d71d96bd5eca5452be795c8034031a58015a
Denis Artyushin <artyushinden@gmail.com>
2023-03-23 14:37:35 +0300

    feature: Isort format

--- graphql2python/type/__init__.py
+++ graphql2python/type/__init__.py
@@ -1,13 +1,13 @@
 from .definitions import (
-    GraphQL2PythonFieldSequenceItemType,
-    GraphQL2PythonScalarType,
-    GraphQL2PythonField,
-    GraphQL2PythonObject,
-    GraphQL2PythonInterfaceType,
-    GraphQL2PythonObjectType,
-    GraphQL2PythonEnumValue,
     GraphQL2PythonEnumType,
-    GraphQL2PythonUnionType,
+    GraphQL2PythonEnumValue,
+    GraphQL2PythonField,
+    GraphQL2PythonFieldSequenceItemType,
+    GraphQL2PythonInterfaceType,
+    GraphQL2PythonObject,
+    GraphQL2PythonObjectType,
+    GraphQL2PythonScalarType,
+    GraphQL2PythonUnionType
 )
 
 __all__ = [
